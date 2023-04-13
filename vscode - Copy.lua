4bee28a237d49538589ba3537f4f5c0b58028c65
camerononggg <130627191+camerononggg@users.noreply.github.com>
2023-04-13 15:40:46 +1000

    Update vscode - Copy.lua

--- vscode - Copy.lua
+++ vscode - Copy.lua
@@ -62,19 +62,10 @@ local Library = {
 
 			StrongText = Color3.fromHSV(0, 0, 1),		
 			WeakText = Color3.fromHSV(0, 0, 172/255)
-			
-			},
-		Cool = {
-			Main = Color3.fromRGB(37, 35, 33),
-			Secondary = Color3.fromRGB(65, 63, 63),
-			Tertiary = Color3.fromRGB(237, 94, 38),
-
-			StrongText = Color3.fromHSV(0, 0, 1),		
-			WeakText = Color3.fromHSV(0, 0, 172/255)
 		},
 		Aqua = {
 			Main = Color3.fromRGB(66, 135, 245),
-			Secondary = Color3.fromRGB(65, 63, 63),
+			Secondary = Color3.fromRGB(26, 51, 92),
 			Tertiary = Color3.fromRGB(51, 153, 137),
 
 			StrongText = Color3.fromHSV(0, 0, 1),        
