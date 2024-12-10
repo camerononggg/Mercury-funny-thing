66ebdfe58a97461dea5e6260da72a775e3579e0e
camerononggg <130627191+camerononggg@users.noreply.github.com>
2024-12-10 20:41:54 +1100

    Update vscode - Copy.lua

--- vscode - Copy.lua
+++ vscode - Copy.lua
@@ -68,8 +68,8 @@ local Library = {
 			Secondary = Color3.fromRGB(181, 180, 180),
 			Tertiary = Color3.fromRGB(0, 0, 0),
 
-			StrongText = Color3.fromHSV(255, 0, 1),        
-			WeakText = Color3.fromHSV(0, 172/255, 0)
+			StrongText = Color3.fromHSV(255, 0, 0),        
+			WeakText = Color3.fromHSV(0, 255, 0)
 		},
 		Vaporwave = {},
 		OperaGX = {},
